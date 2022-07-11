8d736cd0a61f7bf499c1e7854d68891e4e75d4c8
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-11 21:02:09 +0200

    Add item created at date and time

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const itemElements = items.map((item) => (
     <div key={item.id}>
       <input type="checkbox" name="" id="" />
-      <span>{item.text}</span>
+      <span>{item.text} ({item.displayCreatedAt()})</span>
       <button type="button">Delete</button>
     </div>
   ));
--- src/models/Item.js
+++ src/models/Item.js
@@ -2,5 +2,10 @@ export class Item {
   constructor(text) {
     this.id = Date.now();
     this.text = text;
+    this.createdAt = Date.now();
+  }
+
+  displayCreatedAt() {
+    return new Date(this.createdAt).toLocaleString();
   }
 }
