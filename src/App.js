3f0e7ff28e49fb516fde19674a333911f348255f
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2022-07-11 19:58:27 +0200

    Add ItemForm component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { ItemForm } from "./components/ItemForm";
+
 function App() {
   return (
-    <div>TODO app</div>
+    <div>
+      <div>
+        <ItemForm />
+      </div>
+      <div>
+        <div>
+          <input type="checkbox" name="" id="" />
+          <span>Item #1</span>
+          <button type="button">Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/ItemForm/ItemForm.jsx
@@ -0,0 +1,8 @@
+export function ItemForm(props) {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
